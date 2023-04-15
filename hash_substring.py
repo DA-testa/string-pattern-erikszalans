c6e6c80f6e70b993003cd8d8ccaa0eb1383b6ed7
erikszalans <125026714+erikszalans@users.noreply.github.com>
2023-04-15 17:05:21 +0000

    commit

--- hash_substring.py
+++ hash_substring.py
@@ -4,11 +4,11 @@ def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     if 'I' in input():
-        patt =  input().strip()
-        text = input().strip()
-    if 'F' in input():
-        file = open("tests/06","r")
+        patt =  input()
+        text = input()
 
+    if 'F' in input():
+        file = open("./tests/06","r")
         patt = file.readline().strip()
         text = file.readline()
         file.close() 
@@ -20,7 +20,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (patt,text)
+    return (patt.rstrip(),text.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -29,16 +29,14 @@ def print_occurrences(output):
 
 def get_occurrences(patt, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-    pirmais = len(text)
-    otrais = len(patt)
-    tresais = sum(ord(c) for c in patt)
     index = []
+    pattern = hash(patt)
+    
+    for i in range (len(text) - len(patt) + 1):
+        texthash = hash(text[i: i + len(patt)])
 
-    for i in range(pirmais - otrais + 1):
-        ceturtais = sum(ord(text[i + j])for j in range(otrais))
-        
-        if tresais == ceturtais:
-            if text[i : i + otrais] == patt:
+        if texthash == pattern:
+            if text[i:i + len(patt)] == patt:
                 index.append(i)
     # and return an iterable variable
     return index
